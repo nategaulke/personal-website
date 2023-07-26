543bce45e849136b69335e1bbdbacd3642637a46
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-07-25 21:30:50 -0400

    Add Experience section outline

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Intro from "./sections/Intro";
 import About from "./sections/About";
+import Experience from "./sections/Experience";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Intro />
       <About />
+      <Experience />
       <Footer />
     </div>
   );
---
+++ src/sections/Experience.tsx
@@ -0,0 +1,12 @@
+function Experience() {
+  return (
+    <div className="relative h-screen bg-white bg-cover">
+      <div className="mx-60 mt-12 text-black">
+        <h2 className="text-6xl font-bold">experience.</h2>
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default Experience;

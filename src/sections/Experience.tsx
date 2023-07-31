95e68a93d825069dabc379cc9dbb6288388a2237
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-07-31 00:14:14 -0400

    Add headless dependency and experience modal prototype

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,9 +1,12 @@
+import ExperienceModal from "../components/experience/ExperienceModal";
+
 function Experience() {
   return (
     <div className="relative h-screen bg-white bg-cover">
       <div className="mx-60 mt-12 text-black">
         <h2 className="text-6xl font-bold">experience.</h2>
         <br />
+        <ExperienceModal />
       </div>
     </div>
   );
--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -8,10 +8,10 @@ function Intro() {
         <div className="ml-60 h-60 w-60 rounded-full border-4 border-white bg-[url('/profile.png')] bg-contain" />
         <div className="ml-24 text-5xl text-white ">
           <h1>Hi!</h1>
-          <h1>I'm Nathanael</h1>
+          <h1>My name is Nathanael</h1>
           <h1>But you can call me Nate :)</h1>
           {/* <TypeAnimation
-            sequence={[3000, "I'm Nathanael\nBut you can call me Nate :)"]}
+            sequence={[3000, "I'm Nathaznael\nBut you can call me Nate :)"]}
             style={{ whiteSpace: "pre-line", display: "block" }}
           /> */}
         </div>

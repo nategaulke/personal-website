f619c6b8e17456fdeed0eb9a920a1079310ab143
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-07-31 22:49:22 -0400

    Add image, resize modal

--- src/components/experience/ExperienceModal.tsx
+++ src/components/experience/ExperienceModal.tsx
@@ -19,7 +19,7 @@ function ExperienceModal() {
           <img
             className="w-60 rounded-lg opacity-50"
             src="/schwab_logo.svg"
-            alt="BigCo Inc. logo"
+            alt="Schwab Logo"
           />
           <h3 className="mt-2 text-xl font-bold">Schwab</h3>
         </button>
@@ -50,25 +50,39 @@ function ExperienceModal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-midnight p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="h3" className="text-lg text-white">
-                    Associate Software Engineer
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Got it, thanks!
-                    </button>
+                <Dialog.Panel className="w-full max-w-5xl transform overflow-hidden rounded-2xl border-4 border-cyan-500 bg-midnight p-8 text-left align-middle shadow-xl transition-all">
+                  <div className="flex">
+                    <div>
+                      <img
+                        className="mr-8 w-60 rounded-lg"
+                        src="/schwab_logo.svg"
+                        alt="Schwab Logo"
+                      />
+                    </div>
+                    <div>
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-bold text-white"
+                      >
+                        Associate Software Engineer
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Your payment has been successfully submitted. We’ve
+                          sent you an email with all of the details of your
+                          order.
+                        </p>
+                      </div>
+                      <div className="mt-4">
+                        <button
+                          type="button"
+                          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                          onClick={closeModal}
+                        >
+                          Got it, thanks!
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>

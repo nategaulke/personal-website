699f784c24156744e5e3894b7b9a093e4ffde50d
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-09-08 16:16:34 -0400

    Change header for desktop vs mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import Header from "./components/layout/Header";
+import Header from "./components/layout/Header";
 import MobileHeader from "./components/layout/MobileHeader";
 import Footer from "./components/layout/Footer";
 import Intro from "./sections/Intro";
@@ -6,11 +6,12 @@ import About from "./sections/About";
 import Experience from "./sections/Experience";
 import Contact from "./sections/Contact";
 import Projects from "./sections/Projects";
+import { isMobile } from "react-device-detect";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col justify-between">
-      <MobileHeader />
+      {isMobile ? <MobileHeader /> : <Header />}
       <Intro />
       <About />
       <Experience />

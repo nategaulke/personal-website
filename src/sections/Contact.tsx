ac1c8082e79b7815430eb564b196d98e60d17911
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-09-10 20:17:59 -0400

    Replace png with icons for speed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,22 +1,16 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 function Contact() {
   return (
     <div id="contact" className="relative bg-white bg-cover">
       <div className="mt-16 text-black">
-        <h2 className="ml-16 text-6xl font-bold sm:mx-6">contact.</h2>
+        <h2 className="ml-16 text-6xl font-bold sm:mx-60">contact.</h2>
         <div className="my-12 flex justify-center">
           <a href="https://github.com/nategaulke">
-            <img
-              className="mr-12 h-24 rounded-full hover:opacity-50"
-              src="/external-icons/github.png"
-              alt="Github"
-            />
+            <FaGithub className="mr-12 aspect-square rounded text-8xl hover:opacity-50" />
           </a>
           <a href="https://www.linkedin.com/in/nategaulke/">
-            <img
-              className="ml-12 h-24 rounded hover:opacity-50"
-              src="/external-icons/linkedin.png"
-              alt="LinkedIn"
-            />
+            <FaLinkedin className="ml-12 aspect-square rounded text-8xl hover:opacity-50" />
           </a>
         </div>
       </div>

82eede480504b3ef054d550cf042b69bad683530
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-11-06 14:16:30 -0500

    Readjust experience button for mobile experience

--- src/components/experience/ExperienceButton.tsx
+++ src/components/experience/ExperienceButton.tsx
@@ -2,9 +2,9 @@ import TechPill from "./TechPill";
 
 function ExperienceButton(props: any) {
   return (
-    <div className="z-10 flex overflow-hidden text-ellipsis rounded-lg border border-2 bg-white p-4 shadow duration-200 hover:shadow-none sm:w-[80vw] lg:w-[40vw]">
+    <div className="z-10 w-max overflow-hidden text-ellipsis rounded-lg border border-2 bg-white p-4 shadow duration-200 hover:shadow-none sm:flex sm:w-[80vw] sm:w-auto lg:w-[40vw]">
       <div>
-        <div className="w-max">
+        <div className="flex justify-center">
           <img
             className="aspect-sqaure h-40 rounded-lg"
             src={props.logo}
@@ -12,7 +12,8 @@ function ExperienceButton(props: any) {
           />
         </div>
       </div>
-      <div className="ml-4 flex flex-col justify-between">
+      {/* flex h-full w-auto flex-col items-center sm:w-1/3 */}
+      <div className="ml-0 mt-4 flex flex-col justify-between sm:ml-4 sm:mt-0">
         <div>
           <h3 className="text-md font-bold text-black sm:text-left md:text-lg">
             {props.companyName} - {props.jobTitle}
@@ -21,14 +22,14 @@ function ExperienceButton(props: any) {
             {props.timePeriod}
           </h4>
           <div className="mt-2">
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap justify-center sm:justify-normal">
               {props.devTools.map((devTool: any, index: any) => (
                 <TechPill name={devTool} index={index} />
               ))}
             </div>
           </div>
         </div>
-        <div className="justify-end text-left text-blue-700 hover:underline">
+        <div className="text-center text-blue-700 hover:underline sm:justify-end sm:text-left">
           See More
         </div>
       </div>

500664e3f8db29fe863727872258f9d3e7fbf4df
Nathanael Gaulke <nat.gaulke@gmail.com>
2023-11-10 15:11:31 -0500

    Begin adding dedicated json files for the content

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,10 +1,9 @@
 import { lazy } from "react";
+import list from "../content/experience.json";
 
 const ExperienceModal = lazy(
   () => import("../components/experience/ExperienceModal")
 );
-
-const devTools: string[] = ["Java", "Spring"];
 
 function Experience() {
   return (
@@ -20,26 +19,22 @@ function Experience() {
           <div className="absolute left-0 right-0 h-full w-full items-center justify-center">
             <div className="mx-auto flex h-full w-[2px] flex-col overflow-y-auto bg-gray-500" />
           </div>
-          <div className="w-min self-center md:self-start">
-            <ExperienceModal
-              companyName="Schwab"
-              jobTitle="Associate Software Engineer"
-              timePeriod="Jun. 2023 - Present"
-              description="I started worked at Charles Schwab as a software developer intern from June 2022 to August 2022. I enjoyed the company culture of Charles Schwab and learned a lot in those nine weeks, thanks to my manager and technical advisors. When I was offered a role to come back full time after graduation, I was happy to accept. While my team is based primarily in Phoenix, Arizona, I work remotely in Orlando, Florida. My role in the firm is in Retirement Business Services. I have done different dev tasks such as writing new API endpoints, scripting test automation, and managing servers."
-              logo="/schwab_logo.png"
-              devTools={devTools}
-            />
-          </div>
-          <div className="w-min self-center md:self-end">
-            <ExperienceModal
-              companyName="Schwab"
-              jobTitle="Associate Software Engineer"
-              timePeriod="Jun. 2023 - Present"
-              description="I started worked at Charles Schwab as a software developer intern from June 2022 to August 2022. I enjoyed the company culture of Charles Schwab and learned a lot in those nine weeks, thanks to my manager and technical advisors. When I was offered a role to come back full time after graduation, I was happy to accept. While my team is based primarily in Phoenix, Arizona, I work remotely in Orlando, Florida. My role in the firm is in Retirement Business Services. I have done different dev tasks such as writing new API endpoints, scripting test automation, and managing servers."
-              devTools={devTools}
-              logo="/schwab_logo.png"
-            />
-          </div>
+          {list.map((experience, index) => (
+            <div
+              className={`w-min self-center ${
+                index % 2 == 0 ? "md:self-start" : "md:self-end"
+              }`}
+            >
+              <ExperienceModal
+                companyName={experience.companyName}
+                jobTitle={experience.jobTitle}
+                timePeriod={experience.timePeriod}
+                description={experience.description}
+                logo={experience.logo}
+                devTools={experience.devTools}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
